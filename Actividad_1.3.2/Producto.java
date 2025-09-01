7665b11da9b9247f65f0eb6b56b58a84c9dbeda4
Ncatalan <89161301+Ncatalanv@users.noreply.github.com>
2025-09-01 11:59:07 -0400

    Create Producto.java

---
+++ Actividad_1.3.2/Producto.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase;
+
+/**
+ *
+ * @author Cetecom
+ */
+public class Producto {
+    private String idProducto, nombre;
+    private double precio;
+
+    public Producto() {
+    }
+
+    public Producto(String idProducto, String nombre, double precio) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(String idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "idProducto=" + idProducto + ", nombre=" + nombre + ", precio=" + precio + '}';
+    }
+    
+   
+    
+}
