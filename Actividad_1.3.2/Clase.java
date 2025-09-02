c344b185f561e8041b5775ba9a93d5999da1ea5c
Ncatalan <89161301+Ncatalanv@users.noreply.github.com>
2025-09-02 11:51:28 -0400

    Update Clase.java

--- Actividad_1.3.2/Clase.java
+++ Actividad_1.3.2/Clase.java
@@ -4,8 +4,6 @@
  */
 package clase;
 
-import java.util.Scanner;
-
 /**
  *
  * @author Cetecom
@@ -16,46 +14,15 @@ public class Clase {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Producto producto1 = new Producto("001", "Notebook", 170000);
+        Producto producto2 = new Producto("002", "Mouse", 10000);
+        Producto producto3 = new Producto("003", "Teclado", 35000);
+        Cliente cliente1 = new Cliente("123abc", "Ignacio");
         
-        Producto producto1 = new Producto("abc1", "cereal", 4000);
-        CarritoDeCompras carro = new CarritoDeCompras(producto1.toString(), 0);
-        Cliente cliente1 = new Cliente("b123", "Ignacio", carro);
-        
-        cliente1.getCarro().getTotal();
-        
-       
-        
-        Scanner teclado = new Scanner(System.in);
-        
-        int opc;
-        
-        do{
-            System.out.println("--- Tienda DuocUC ---");
-            System.out.println("1.- Ver catálogo de productos");
-            System.out.println("2.- Ver carrito de compras.");
-            System.out.println("3.- Salir");
-            
-            opc = teclado.nextInt();
-            
-            switch(opc){
-                case 1: 
-                    System.out.println("Productos disponibles");
-                    System.out.println(producto1.toString());
-                case 2:
-                    System.out.println("Carrito de Compras");
-                case 3:
-                    System.out.println("Saliendo de la tienda...");
-                    break;
-                default:
-                    System.out.println("Opción no válida.");
-                          
-            
-        }
-        } while (opc !=5);
-                
-        
-             
-
+        cliente1.comprarProducto(producto1);
+        cliente1.comprarProducto(producto3);
+        cliente1.comprarProducto(producto1);
+        cliente1.verCarrito();
     }
     
 }
